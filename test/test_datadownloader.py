45e996ab0b3647fab1277f998537cf5fa4800524
Priyanka Mehta <34035008+priyankaMehta1@users.noreply.github.com>
2021-09-29 11:27:45 -0400

    Update downloader path

--- test/test_datadownloader.py
+++ test/test_datadownloader.py
@@ -2,4 +2,4 @@ import pytest
 from datadownloader import DataDownloader
 
 def test_downloader():
-  assert(DataDownloader.getPath() != "")
+  assert(DataDownloader.getPath() != "./Places2 Dataset")
