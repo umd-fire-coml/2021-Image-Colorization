796e4133b516b64f7a55837ce209d3dde4a7bca7
Matthew Snyder <msnyder2@terpmail.umd.edu>
2021-10-06 10:07:26 -0400

    Changed to tensorflow

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,15 +1,32 @@
+import numpy as np
+from tensorflow.keras.utils import Sequence
 import os
-from torch.utils.data import DataLoader
+import cv2
 
-class DataLoader():
-    def __init__(self, train_path = r'./Places2 Dataset/Train', test_path = r'./Places2 Dataset/Test'):
-        for subdir, dirs, files in os.walk(train_path):
-            training_data = files
-        for subdir, dirs, files in os.walk(test_path):
-            testing_data = files
-        self.train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
-        self.test_dataloader = DataLoader(testing_data, batch_size=64, shuffle=True)
-        for batch in self.train_dataloader:
-            print(batch.shape())
-        for batch in self.train_dataloader:
-            print(batch.shape())
+class DataGenerator(Sequence):
+    def __init__(self, file_path, batch_size=8, x_shape=(256, 256, 3), y_shape=(10,)):
+        self.batch_size = batch_size
+        self.x_shape = x_shape
+        self.y_shape = y_shape
+        self.im_paths = os.listdir(file_path)
+        self.indexes = np.arange(len(self.im_paths))
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.floor(len(self.im_paths) / self.batch_size))
+
+    def __getitem__(self, index):
+        x_batch = np.empty((self.batch_size, *self.x_shape))
+        y_batch = np.empty((self.batch_size, *self.y_shape))
+
+        indexes = self.indexes[index * self.batch_size : (index + 1) * self.batch_size]
+
+        for i in range(self.batch_size):
+            image = self.im_paths[indexes[i]]
+            x_batch[i,] = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            y_batch[i,] = image
+
+        return x_batch, y_batch
+
+    def on_epoch_end(self):
+        np.random.shuffle(self.indexes)
