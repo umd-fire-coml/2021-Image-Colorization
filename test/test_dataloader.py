2a7c27f7d4a53795470124540e9812d3d05ed5cf
Matthew Snyder <msnyder2@terpmail.umd.edu>
2021-10-13 11:33:43 -0400

    changed tests

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -7,5 +7,5 @@ def test_dataloader():
     assert(data_loader.batch_size == 8)
     assert(data_loader.x_shape == (256, 256, 3))
     assert(data_loader.y_shape == (256, 256, 3))
-    assert(isinstance(data_loader.im_paths, list))
+    assert(isinstance(data_loader.im_paths, list[str]))
     assert(isinstance(data_loader.indexes, numpy.array))
