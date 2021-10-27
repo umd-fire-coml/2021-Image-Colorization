b136e4c8341fb72d8c0ffbc5bc9f7c64e41cec44
Varun Ayyappan <varunayyappan@gmail.com>
2021-10-27 11:34:34 -0400

    Made the model object a Sequential Model

--- src/model.py
+++ src/model.py
@@ -4,26 +4,22 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 
-class Model(object):
+class Model(Sequential):
     def __init__(self, training, validation):
-        self.model = self.my_model()
+        self.my_model()
         self.training_loader = training
         self.validation_loader = validation
 
     # Create a model for training
     def my_model(self):
-        model = Sequential()
-
-        model.add(Dense(5, activation='relu', input_shape=(4,)))
-        model.add(Dense(10, activation='relu'))
-        model.add(Dense(3, activation='softmax'))
-
-        return model
+        self.add(Dense(5, activation='relu', input_shape=(4,)))
+        self.add(Dense(10, activation='relu'))
+        self.add(Dense(3, activation='softmax'))
 
     # Train the model
     def train_model(self):
-        self.model.compile(
+        self.compile(
             optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
-        history = self.model.fit(
+        history = self.fit(
             self.training_loader, validation_data=self.validation_generator)
