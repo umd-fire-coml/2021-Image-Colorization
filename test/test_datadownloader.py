b941db1b6dc32eb041ba2298365c041daeeabbf4
Varun Ayyappan <varunayyappan@gmail.com>
2021-12-09 15:45:11 -0500

    Add setup.sh, fixed tests, export data visualizer

--- test/test_datadownloader.py
+++ test/test_datadownloader.py
@@ -7,5 +7,5 @@ def test_downloader():
   url = 'http://ipv4.download.thinkbroadband.com/5MB.zip'
   save_path = '../Dataset'
   dataset_name = 'test'
-  download_url(url, dataset_name, save_path)
+  download_url(url, dataset_name)
   assert(len(os.listdir(save_path)))
