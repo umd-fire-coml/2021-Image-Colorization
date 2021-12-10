abc7b6f301fe6f90fc0b8efa2018dd5613ca8d22
Varun Ayyappan <varunayyappan@gmail.com>
2021-12-09 19:12:46 -0500

    fix tests

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+# Setup Environment
+python3 -m venv imagecolorization-env
+source imagecolorization-env/bin/activate
+
 # Installation Reqs
 pip install -r requirements.txt
 pip install -r test-requirements.txt
--- test/test_datadownloader.py
+++ test/test_datadownloader.py
@@ -4,8 +4,8 @@ from src.datadownloader import *
 
 
 def test_downloader():
-  url = 'http://ipv4.download.thinkbroadband.com/5MB.zip'
-  save_path = '../Dataset'
+  url = 'https://www.learningcontainer.com/download/sample-tar-file-download-for-testing/?wpdmdl=2487&refresh=61754a613701e1635076705'
+  save_path = './data'
   dataset_name = 'test'
-  download_url(url, dataset_name)
+  download_url(url, dataset_name, save_path)
   assert(len(os.listdir(save_path)))
